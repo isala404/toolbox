9f9ca28813c463e08d743af3f7e4938ddb572688
Isala Piyarisi <mail@isala.me>
2024-08-01 16:31:32 +0530

    Add support for RST endpoint in network debug service

--- network-debug/main.py
+++ network-debug/main.py
@@ -1,5 +1,8 @@
 import socket
 import threading
+import struct
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
 
 # TCP Echo Server
 def tcp_echo_server(host='0.0.0.0', port=5600):
@@ -29,12 +32,52 @@ def udp_echo_server(host='0.0.0.0', port=5700):
         print(f'Received data from {addr}: {data.decode()}')
         sock.sendto(data, addr)  # Echo the received data back
 
+# HTTP Server with Reset Endpoint
+class ResetHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path.startswith('/reset'):
+            parsed_url = urllib.parse.urlparse(self.path)
+            query_params = urllib.parse.parse_qs(parsed_url.query)
+            
+            do_param = query_params.get('do', ['false'])[0].lower()
+            
+            if do_param == 'true':
+                # Send RST packet
+                self.send_reset()
+            else:
+                # Return 200 OK
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(b"OK")
+
+    def send_reset(self):
+        # Get the client socket
+        client_socket = self.request
+
+        # Enable SO_LINGER with a timeout of 0 to send RST
+        l_onoff = 1
+        l_linger = 0
+        client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, 
+                                 struct.pack('ii', l_onoff, l_linger))
+        
+        # Close the socket to trigger the RST
+        client_socket.close()
+
+def http_server(host='0.0.0.0', port=5800):
+    server_address = (host, port)
+    httpd = HTTPServer(server_address, ResetHandler)
+    print(f'HTTP Server with Reset endpoint listening on {host}:{port}')
+    httpd.serve_forever()
+
 if __name__ == '__main__':
     tcp_server = threading.Thread(target=tcp_echo_server)
     udp_server = threading.Thread(target=udp_echo_server)
+    reset_server = threading.Thread(target=http_server)
 
     tcp_server.start()
     udp_server.start()
+    reset_server.start()
 
     tcp_server.join()
     udp_server.join()
+    reset_server.join()
